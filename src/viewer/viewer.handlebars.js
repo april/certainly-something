da3b2e0b55734e655223af5562ee0a94dcb2461e
April King <april@mozilla.com>
2018-07-03 18:45:31 -0500

    Significant display improvements

--- src/viewer/viewer.handlebars.js
+++ src/viewer/viewer.handlebars.js
@@ -19,30 +19,130 @@ templates['viewer'] = template({"1":function(container,depth0,helpers,partials,d
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.cn : stack1), depth0))
     + " "
     + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.isBuiltInRoot : depth0),{"name":"if","hash":{},"fn":container.program(6, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</h2></div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Subject</div>\n          <div class=\"text\">"
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.path : stack1), depth0))
-    + "</div>\n        </div>\n"
-    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.subjectAltNames : depth0),{"name":"if","hash":{},"fn":container.program(8, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "</h2></div>\n        </div>\n\n        <!-- Subject -->\n        <div class=\"panel-section-subheader\">\n          <span>Subject Name</span>\n        </div>\n"
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.c : stack1),{"name":"if","hash":{},"fn":container.program(8, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.s : stack1),{"name":"if","hash":{},"fn":container.program(10, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.l : stack1),{"name":"if","hash":{},"fn":container.program(12, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.address : stack1),{"name":"if","hash":{},"fn":container.program(14, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.ou : stack1),{"name":"if","hash":{},"fn":container.program(16, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.o : stack1),{"name":"if","hash":{},"fn":container.program(18, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.cn : stack1),{"name":"if","hash":{},"fn":container.program(20, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\n        <!-- Issuer -->\n        <div class=\"panel-section-subheader\">\n          <span>Issuer Name</span>\n        </div>\n"
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.c : stack1),{"name":"if","hash":{},"fn":container.program(22, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.s : stack1),{"name":"if","hash":{},"fn":container.program(24, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.l : stack1),{"name":"if","hash":{},"fn":container.program(26, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.address : stack1),{"name":"if","hash":{},"fn":container.program(28, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.ou : stack1),{"name":"if","hash":{},"fn":container.program(30, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.o : stack1),{"name":"if","hash":{},"fn":container.program(32, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.cn : stack1),{"name":"if","hash":{},"fn":container.program(34, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\n\n        <!-- Validity -->\n        <div class=\"panel-section-subheader\">\n          <span>Validity</span>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Not Before</div>\n          <div class=\"text\">"
+    + alias2(alias1((depth0 != null ? depth0.notBefore : depth0), depth0))
+    + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Not After</div>\n          <div class=\"text\">"
+    + alias2(alias1((depth0 != null ? depth0.notAfter : depth0), depth0))
+    + "</div>\n        </div>\n\n        <div class=\"panel-section-subheader\">\n          <span>Miscellaneous</span>\n        </div>\n"
+    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.subjectAltNames : depth0),{"name":"if","hash":{},"fn":container.program(36, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "        <div class=\"panel-list-item\">\n          <div class=\"text\">Download</div>\n          <div class=\"text\"><a href=\"data:,"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.files : depth0)) != null ? stack1.pem : stack1), depth0))
     + "\" download=\""
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.cn : stack1), depth0))
     + ".pem\">PEM</a></div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Serial Number</div>\n          <div class=\"text\">"
     + alias2(alias1((depth0 != null ? depth0.serialNumber : depth0), depth0))
-    + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Issuer</div>\n          <div class=\"text\">"
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.path : stack1), depth0))
-    + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Valid From</div>\n          <div class=\"text\">"
-    + alias2(alias1((depth0 != null ? depth0.notBefore : depth0), depth0))
-    + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Valid To</div>\n          <div class=\"text\">"
-    + alias2(alias1((depth0 != null ? depth0.notAfter : depth0), depth0))
     + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Version</div>\n          <div class=\"text\">"
     + alias2(alias1((depth0 != null ? depth0.version : depth0), depth0))
     + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">Signature Algorithm</div>\n          <div class=\"text\">"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.signature : depth0)) != null ? stack1.name : stack1), depth0))
-    + "</div>\n        </div>\n      </div>\n    </div>\n";
+    + "</div>\n        </div>\n\n        <!-- Fingerprints -->\n        <div class=\"panel-section-subheader\">\n          <span>Fingerprints</span>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">SHA-256</div>\n          <div class=\"text\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.fingerprint : depth0)) != null ? stack1.sha256 : stack1), depth0))
+    + "</div>\n        </div>\n        <div class=\"panel-list-item\">\n          <div class=\"text\">SHA-1</div>\n          <div class=\"text\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.fingerprint : depth0)) != null ? stack1.sha1 : stack1), depth0))
+    + "</div>\n        </div>\n\n      </div>\n    </div>\n";
 },"6":function(container,depth0,helpers,partials,data) {
     return "(built-in root)";
 },"8":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Country</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.c : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"10":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">State / Province</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.s : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"12":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Locality</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.l : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"14":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Street Address</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.address : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"16":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Organizational Unit</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.ou : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"18":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Organization</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.o : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"20":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Common Name</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.subject : depth0)) != null ? stack1.cn : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"22":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Country</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.c : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"24":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">State / Province</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.s : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"26":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Locality</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.l : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"28":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Street Address</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.address : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"30":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Organizational Unit</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.ou : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"32":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Organization</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.o : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"34":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Common Name</div>\n          <div class=\"text\">"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.issuer : depth0)) != null ? stack1.cn : stack1), depth0))
+    + "</div>\n        </div>\n";
+},"36":function(container,depth0,helpers,partials,data) {
     return "        <div class=\"panel-list-item\">\n          <div class=\"text\">Subject Alt Names</div>\n          <div class=\"text\">"
     + container.escapeExpression(container.lambda((depth0 != null ? depth0.subjectAltNames : depth0), depth0))
     + "</div>\n        </div>        \n";
