9bb18516f316328b16cae22f39aabeb1f3bf0c99
April King <april@mozilla.com>
2018-07-19 11:55:41 -0500

    fix bug with setting state

--- src/background/state.js
+++ src/background/state.js
@@ -1,9 +1,5 @@
 const _state = {};
 
-// const log = (from) => {
-//   console.log('current tab state is', _state, 'from', from);
-// }
-
 export const init = (tid) => {
   _state[tid] = {
     si: {
@@ -18,14 +14,13 @@ export const get = (tid) => {
 
 export const remove = (tid) => {
   if (_state.hasOwnProperty(tid)) {
-    console.log('hello');
     delete _state[tid];
   }
 }
 
 export const set = (tid, state) => {
   if (typeof state === 'string') {         // like 'secure', 'http', etc.
-    _state.state = state;
+    _state[tid].state = state;
   } else if (typeof state === 'object') {  // setting security info
     _state[tid] = state;
   }
