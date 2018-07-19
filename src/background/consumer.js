9bf11272d45f167859152441861f2157dacd7e82
April King <april@mozilla.com>
2018-07-19 11:56:12 -0500

    Fix bug with downgrading state

--- src/background/consumer.js
+++ src/background/consumer.js
@@ -2,7 +2,7 @@ import * as state from './state';
 
 // state can only be downgraded, not upgraded
 const getWorseState = (tid, newState) => {
-  const curState = tabState[tid].si.state;
+  const curState = state.get(tid).state;
   const states = ['secure', 'insecure', 'broken', 'http'];  // HTTPS, invalid HTTPS, broken/mixed content, HTTP
 
   if (states.indexOf(newState) > states.indexOf(curState)) {
@@ -46,10 +46,8 @@ export const consume = async details => {
 
   // update document state depending on various conditions
   if (documentUrl.protocol === 'https:' && url.protocol === 'http:') {  // mixed content
-    // tabState[tid].si.state = getWorseState(tid, 'broken');
     state.set(tid, getWorseState(tid, 'broken'));
   } else if (url.protocol === 'http:') {  // plain HTTP
     state.set(tid, getWorseState(tid, 'http'));
-    // tabState[tid].si.state = getWorseState(tid, 'http');
   }
 };
