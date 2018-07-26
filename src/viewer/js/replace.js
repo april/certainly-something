503ef71ee12b5341eaf1392658b1f246465e252c
April King <april@mozilla.com>
2018-07-26 10:43:08 -0500

    Remove errant js file

--- src/viewer/js/replace.js
+++
@@ -1,6 +0,0 @@
-export default (str, a, b) => {
-  if (typeof str !== 'string') return '';
-  if (typeof a !== 'string') return str;
-  if (typeof b !== 'string') b = '';
-  return str.split(a).join(b);
-};
